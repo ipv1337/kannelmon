9187191b61aafd1b6870378bab3ace9af0631752
eduardoraad <eduardoraad@f5eeea9b-0a50-0410-a5f2-ad3a954532f7>
2008-06-19 20:44:59 +0000

    new project started

---
+++ trunk/trunk/xmlparser.py
@@ -0,0 +1,46 @@
+from xml.parsers import expat
+
+class Parser:
+
+    def __init__(self):
+        self._parser = expat.ParserCreate()
+        self._parser.StartElementHandler = self.start
+        self._parser.EndElementHandler = self.end
+        self._parser.CharacterDataHandler = self.data
+        self.inSmsc = 0
+        self.inId = 0
+        self.inQueued = 0
+        self.mapping = {}
+
+    def feed(self, data):
+        if data:
+            self._parser.Parse(data, 0)
+            return self.mapping
+
+    def close(self):
+        self._parser.Parse("", 1) # end of data
+        del self._parser # get rid of circular references
+
+    def start(self, tag, attr):
+        if tag == 'smsc':
+            self.inSmsc = 1
+        elif tag == 'id':
+            self.inId= 1
+        elif tag == 'queued':
+            self.inQueued= 1
+            
+    def data(self, data):
+        if self.inSmsc:
+            if self.inId:
+                self.smscId = data
+            elif self.inQueued:
+                self.queued = data
+
+    def end(self, tag):
+        if self.inId:
+            self.inId = 0
+        elif self.inQueued:
+            self.inQueued = 0
+        elif self.inSmsc:
+            self.inSmsc = 0
+            self.mapping['hola'] = self.queued
---
+++ trunk/trunk/xmlparser2.py
@@ -0,0 +1,50 @@
+from xml.parsers import expat
+
+class Parser:
+
+    def __init__(self):
+        self._parser = expat.ParserCreate()
+        self._parser.StartElementHandler = self.start
+        self._parser.EndElementHandler = self.end
+        self._parser.CharacterDataHandler = self.data
+        self.inStatus = 0
+        self.inSmsc = 0
+        self.inId = 0
+        self.status = ''
+        self.id = ''
+        self.lastId = ''
+        self.mapping = {}
+        self.properties = {}
+
+    def feed(self, data):
+        if data:
+            self._parser.Parse(data, 0)
+            return self.mapping
+
+    def close(self):
+        self._parser.Parse("", 1) # end of data
+        del self._parser # get rid of circular references
+
+    def start(self, tag, attr):
+        if tag == 'smsc':
+            self.inSmsc = 1
+        elif tag == 'status':
+            self.inStatus = 1
+        elif tag == 'id':
+            self.inId = 1
+            
+    def data(self, data):
+        if self.inSmsc:
+            if self.inStatus:                self.status = data
+            if self.inId:
+                self.id = data
+                self.lastId = self.id
+
+    def end(self, tag):
+        if tag == 'id':
+            self.inId = 0
+        elif tag == 'status':
+            self.inStatus = 0
+        elif tag == 'smsc':
+            self.inSmsc = 0
+            self.mapping[self.lastId] = self.status
